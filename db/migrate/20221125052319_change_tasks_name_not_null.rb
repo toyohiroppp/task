6f0ba21ae16bf59c5cd92918e55310430fdd01ab
toyohiroppp <fix.jpmoy@gmail.com>
2022-11-25 23:39:25 +0900

    add_validates

--- db/migrate/20221125052319_change_tasks_name_not_null.rb
+++ db/migrate/20221125052319_change_tasks_name_not_null.rb
@@ -1,5 +1,13 @@
 class ChangeTasksNameNotNull < ActiveRecord::Migration[5.2]
   def change
-    change_column_null :tasks, :name, false
+    add_index :tasks, :name, unique: true
+  end
+
+  def up
+    change_column :tasks, :name, :string, limit: 30
+  end
+
+  def down
+    change_column :tasks, :name, :string
   end
 end
