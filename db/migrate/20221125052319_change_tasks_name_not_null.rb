39c0b4a9d55ab9be5cfd56758feee5c9a999496c
toyohiroppp <fix.jpmoy@gmail.com>
2022-12-07 16:52:49 +0900

    Chapter4

--- db/migrate/20221125052319_change_tasks_name_not_null.rb
+++ db/migrate/20221125052319_change_tasks_name_not_null.rb
@@ -1,13 +1,5 @@
 class ChangeTasksNameNotNull < ActiveRecord::Migration[5.2]
   def change
-    add_index :tasks, :name, unique: true
-  end
-
-  def up
-    change_column :tasks, :name, :string, limit: 30
-  end
-
-  def down
-    change_column :tasks, :name, :string
+    change_column_null :tasks, :name, false
   end
 end
